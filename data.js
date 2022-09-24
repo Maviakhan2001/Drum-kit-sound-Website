ada3d58eb629e1d49fa3a6e4a1bdff807039cf63
Mavia Khan <113515670+Maviakhan2001@users.noreply.github.com>
2022-09-24 21:02:41 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'lorem 1',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'Images/slider 2.PNG'
+    },
+    {
+        name: 'lorem 2',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'Images/slider 1.PNG'
+    },
+    {
+        name: 'lorem 3',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'Images/slider 3.PNG'
+    },
+    {
+        name: 'Lorem 4',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'Images/slider 4.PNG'
+    },
+    {
+        name: 'Lorem 5',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'Images/slider 5.PNG'
+    }
+]
